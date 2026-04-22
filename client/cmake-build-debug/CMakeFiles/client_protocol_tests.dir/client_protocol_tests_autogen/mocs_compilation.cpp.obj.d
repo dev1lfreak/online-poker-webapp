1c94cfae2f8dc856ebe9b3c004743da2933ec161
dev1lfreak <kvachuk.sergey@yandex.ru>
2026-04-22 15:11:29 +0300

    fix : delete cmake-build-debug

--- client/cmake-build-debug/CMakeFiles/client_protocol_tests.dir/client_protocol_tests_autogen/mocs_compilation.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/client_protocol_tests.dir/client_protocol_tests_autogen/mocs_compilation.cpp.obj: \
- D:\ITMO\multithreads\online-poker-webapp\client\cmake-build-debug\client_protocol_tests_autogen\mocs_compilation.cpp
